59439f2ae680b3331a157eccda1b224c55992ea3
Lenovo <rusobraz@mail.ru>
2023-10-24 15:53:45 +0300

    Инициализация ДЗ

---
+++ src/test/java/com/example/homework_2_11/HomeWork211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
